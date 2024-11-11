c38ba6dab35151356739fa6cb014cd09af3e7c8d
kumarprince06 <princekumarkolkata18@gmail.com>
2024-11-11 22:19:05 +0530

    Homepage, header, footer design completed

--- src/main/java/com/example/OnlineQuizApp/controller/PageController.java
+++ src/main/java/com/example/OnlineQuizApp/controller/PageController.java
@@ -1,14 +1,21 @@
 package com.example.OnlineQuizApp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/online-quiz")
 public class PageController {
 
+    @RequestMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Home");
+        return "index";
+    }
+
     @RequestMapping("/home")
-    public String index(){
-       return "index";
+    public String index(Model model) {
+        model.addAttribute("title", "Home");
+        return "index";
     }
 }
